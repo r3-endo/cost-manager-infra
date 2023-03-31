b367b10ef5c906320bc5c1c5df958852a9b3d540
rocorock <t.o94th@gmail.com>
2023-03-31 17:07:03 +0900

    VPCの追加

--- terraform/project/variable.tf
+++ terraform/project/variable.tf
@@ -1,8 +1,10 @@
 variable "aws_account_id" {}
 variable "aws_region" {}
-variable "environment" {
-  default = "common"
-}
-variable "project" {
-  default = "cost-manager"
+
+variable "tags" {
+  type = map(string)
+  default = {
+    system = "cost-manager"
+    project = "cost-manager"
+  }
 }
---
+++ terraform/project/vpc.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "name" {
+  cidr_block = local.vpc_cidr
+  enable_dns_hostnames = "true"
+  enable_dns_support = "true"
+  enable_classiclink = "false"
+  enable_classiclink_dns_support = "false"
+
+  tags = merge(
+    tomap({
+        "Name": "${var.tags["system"]}-vpc"
+        "project": "${var.tags["project"]}"
+    }),var.tags)
+}
