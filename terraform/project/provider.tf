b8e53944159bf43657b2b415f8f50a453bd3feb9
rocorock <t.o94th@gmail.com>
2023-03-31 15:54:31 +0900

    actionの追加

---
+++ terraform/project/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "~> 1.4.4"
+
+  backend "s3" {
+    bucket         = "cost-manager-tf-state-bucket"
+    region         = "ap-northeast-1"
+    key            = "common-github.tfstate"
+    dynamodb_table = "cost-manager-tf-state-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.47.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      env     = var.environment
+      project = var.project
+      owner   = "terraform"
+    }
+  }
+}
